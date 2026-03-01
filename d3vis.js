d8b0f639c506719c065f045ac901863e7222b0cc
MaxMcCalla <36572894+MaxMcCalla@users.noreply.github.com>
2026-03-01 18:35:58 -0500

    Added the bar graph of quotes by year

---
+++ d3vis.js
@@ -0,0 +1,73 @@
+// Declare the chart dimensions and margins.
+  const width = 1300;
+  const height = 550;
+  const marginTop = 300;
+  const marginRight = 40;
+  const marginBottom = 30;
+  const marginLeft = 40;
+
+  d3.csv("../data/onion_data.csv",d3.autoType).then(function(data) {
+
+
+    data.forEach( function (d){
+        d.year = d.date.getFullYear();
+    });
+
+//    const bins = d3.bin().value((d) => d.year).thresholds(d => d3.max(d.year)-d3.min(d.year))(data);
+
+    function thresholdTime(n) {
+    return (data, min, max) => {
+        return d3.scaleUtc().domain([min, max]).ticks(n);
+    };
+    }
+
+    const bins = d3.bin()
+    .value(d => d.date)
+    .thresholds(thresholdTime(30))
+    (data)
+
+    console.log(bins);
+// Declare the x (horizontal position) scale.
+  const x = d3.scaleLinear()
+      .domain([bins[0].x0.getFullYear(), bins[bins.length - 1].x1.getFullYear()])
+      .range([marginLeft, width - marginRight]);
+
+  // Declare the y (vertical position) scale.
+  const y = d3.scaleLinear()
+      .domain([0, d3.max(bins, (d) => d.length)])
+      .range([height - marginBottom, marginTop]);
+
+  // Create the SVG container.
+  const svg = d3.create("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [0, 0, width, height])
+      .attr("style", "max-width: 100%; height: auto;");
+
+  // Add a rect for each bin.
+  svg.append("g")
+      .attr("fill", "gray")
+    .selectAll()
+    .data(bins)
+    .join("rect")        
+        .on("mouseover",function(event,d){
+            d3.select(this)
+                .attr("fill","black");
+        })
+        .on("mouseout",function(event,d){
+            d3.select(this)
+                .attr("fill","gray");
+        })
+      .attr("x", (d) => x(d.x0.getFullYear()) + 1)
+      .attr("width", (d) => x(d.x1.getFullYear()) - x(d.x0.getFullYear()) - 1)
+      .attr("y", (d) => y(d.length))
+      .attr("height", (d) => y(0) - y(d.length));
+
+  // Add the x-axis and label.
+  svg.append("g")
+      .attr("transform", `translate(${(x(2023)-x(2022)-1)/2},${height - marginBottom})`)
+      .call(d3.axisBottom(x).ticks(30).tickSizeOuter(0))
+
+
+    vis.append(svg.node());
+    })
